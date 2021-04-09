b81a1127e7699b9c5bfade4b1e75eab2a8068038
christ3444 <christianmonmail34444@gmail.com>
2021-04-09 06:49:33 +0000

    page d'accueil

---
+++ footer.php
@@ -0,0 +1,67 @@
+<footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer__about">
+                        <div class="footer__about__logo">
+                            <a href="./index.php"><img src="img/logo_url.png" width="150px" height="100px"></a>
+                        </div>
+                        <p>lorem ipsum bla bla lom spatum lecul ares</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 offset-lg-1 col-md-6">
+                    <div class="footer__address">
+                        <ul>
+                            <li>
+                                <span>Nous contacter:</span>
+                                <p>(+228) 00-00-00-00</p>
+                            </li>
+                            <li>
+                                <span>Email:</span>
+                                <p>SiteUrl@gmail .com</p>
+                            </li>                          
+                            <li>
+                                <span>nous suivre</span>
+                                <div class="footer__social">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-lg-1 col-md-6">
+                    <div class="footer__widget">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Blog</a></li>
+                        </ul>
+                        <!-- <ul>
+                            <li><a href="#">Sign In</a></li>
+                            <li><a href="#">How it Work</a></li>
+                            <li><a href="#">Advantages</a></li>
+                            <li><a href="#">Direo App</a></li>
+                            <li><a href="#">Packages</a></li>
+                        </ul> -->
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer__copyright">
+                        <div class="footer__copyright__text">
+                            <p>
+                                Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+                                 <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            </p>
+                        </div>
+                        <div class="footer__copyright__links">
+                            <a href="#">Accord de confidencialite</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ header.php
@@ -0,0 +1,67 @@
+<header class="header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-md-3">
+                    <div class="header__logo">
+                       <p style="color:green;">272 taches a faire</p>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-md-9">
+                    <div class="header__nav">
+                        <nav class="header__menu mobile-menu">
+                            <ul>
+                                <li class="active"><a href="./index.php">Accueil</a></li>
+                                <li><a href="#">Entreprises</a>
+                                <li><a href="#">Categories</a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">agence immobilier</a></li>
+                                        <li><a href="#">restoration</a></li>
+                                        <li><a href="#">hotel</a></li>
+                                        <li><a href="#">agence de securite</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">A propos</a></li>
+                                <li><a href="#">Contact</a></li>
+                                
+
+                            </ul>
+                        </nav>
+                        <div class="header__menu__right">
+                            <a href="#" class="primary-btn" data-toggle="modal" data-target="#addlink"><i class="fa fa-plus"></i>Ajouter un lien</a>
+                            <button type="button" class="btn btn-lg btn-dark" data-toggle="modal" data-target="#login">LogIn</button>
+                            <a href="#" class="login-btn"data-toggle="modal" data-target="#dashboard"><i class="fa fa-user"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="mobile-menu-wrap">
+                <div class="slicknav_menu"> 
+                    <!-- <a href="#" aria-haspopup="true" role="button" tabindex="0" class="slicknav_btn slicknav_open" style="outline: none;">
+                        <span class="slicknav_menutxt">MENU</span>
+                            <span class="slicknav_icon"><span class="slicknav_icon-bar"></span>
+                            <span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span>
+                        </span>
+                    </a> -->
+                    <nav class="slicknav_nav" aria-hidden="false" role="menu" style="">
+                         <!--<ul>
+                            <li><a href="#" role="menuitem">Entreprises</a>
+                            </li><li class="slicknav_collapsed slicknav_parent"><a href="#" role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item slicknav_row" style="outline: none;"><a href="#">Categories</a>
+                                <span class="slicknav_arrow">►</span></a><ul class="dropdown slicknav_hidden" role="menu" aria-hidden="true" style="display: none;">
+                                    <li><a href="#" role="menuitem" tabindex="-1">agence immobilier</a></li>
+                                    <li><a href="#" role="menuitem" tabindex="-1">restoration</a></li>
+                                    <li><a href="#" role="menuitem" tabindex="-1">hotel</a></li>
+                                    <li><a href="#" role="menuitem" tabindex="-1">agence de securite</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#" role="menuitem">A propos</a></li>
+                            <li><a href="#" role="menuitem">Contact</a></li> -->
+                            <a href="#" class="primary-btn"><i class="fa fa-plus" data-toggle="modal" data-target="#addlink"></i>Ajouter un lien</a>
+                                <button type="button" class="btn btn-lg btn-dark" data-toggle="modal" data-target="#login">LogIn</button>
+                                <a href="#" type="button" class="login-btn" data-toggle="modal" data-target="#dashboard" ><i class="fa fa-user"></i></a>
+        <!-- 
+                        </ul> -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
